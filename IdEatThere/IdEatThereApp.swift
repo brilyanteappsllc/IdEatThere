d56f093ab18ac0d93ca1bcd18187d3d26a8b5aab
Matthew Mejia <matthew.mejia@brilyanteapps.com>
2023-01-03 21:05:49 -0600

    Initial Commit

---
+++ IdEatThere/IdEatThereApp.swift
@@ -0,0 +1,17 @@
+//
+//  IdEatThereApp.swift
+//  IdEatThere
+//
+//  Created by Brilyante Apps LLC on 1/3/23.
+//
+
+import SwiftUI
+
+@main
+struct IdEatThereApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
