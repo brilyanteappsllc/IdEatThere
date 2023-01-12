db990b5c980483e83c2c765b285a1e28c8b893af
Matthew Mejia <matthew.mejia@brilyanteapps.com>
2023-01-12 10:17:43 -0700

    - test

--- IdEatThere/AppDelegate.swift
+++ IdEatThere/AppDelegate.swift
@@ -15,6 +15,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     
     var window: UIWindow?
     
+    // Test
     
     
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
